28caf1710c86ffa5b9db3acd12bc3bb8a488b58f
Taline Kehlenbach <taline.kehlenbach@rwth-aachen.de>
2021-12-07 22:47:24 +0100

    WIP: citation timeline fixed but neeeds to run

--- citations/preprocessing/create_citation_timeline.py
+++ citations/preprocessing/create_citation_timeline.py
@@ -4,31 +4,34 @@ import numpy as np
 name = "../citation-network-dataset/reference_data.feather"
 df = pd.read_feather(name)
 df = df.replace('nan', np.nan)
-timelines = {}
-stopper = 0
+cols = ['id', 'year', 'timeline']
+timelines = pd.DataFrame(columns=cols)
+
 for idx, paper in df.iterrows():
     paper_id = paper['_id']
     year = paper['year']
-    if paper_id not in timelines.keys():
-        timelines[paper_id] = {}
-    timelines[paper_id]['year'] = year
-    if 'timeline' not in timelines[paper_id].keys():
-        timelines[paper_id]['timeline'] = []
+
+    if paper_id not in timelines['id']:
+        temp = pd.DataFrame([[paper_id, 0, list()]], columns=cols)
+        timelines = timelines.append(temp)
+
+    timelines.loc[timelines['id'] == paper_id, 'year'] = year
+
     refs = paper['ref']
-    print(refs)
+
     if type(refs) == str:
         refs = refs.replace('[', '')
         refs = refs.replace(']', '')
         refs = refs.split(',')
+
         for r in refs:
-            print(r)
-            if r in timelines.keys():
-                timelines[r]['timeline'].append[year]
+
+            if r in timelines['id']:
+                timelines.loc[timelines['id'] == r, 'timeline'] = timelines.at[timelines['id'] == r, 'timeline'].append(
+                    year)
             else:
-                timelines[paper_id] = {}
-                timelines[paper_id]['timeline'] = [year]
-    stopper += 1
-    if stopper == 10:
-        break
-print(pd.DataFrame.from_dict(timelines, orient='index'))
-print(df[['_id', 'year', 'ref']].head(10))
+                temp = pd.DataFrame([[paper_id, 0, list([year])]], columns=cols)
+                timelines = timelines.append(temp)
+    print(idx)
+output = '../citation-network-dataset/timeline_data.feather'
+timelines.to_feather(output)
