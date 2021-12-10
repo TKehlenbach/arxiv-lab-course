1ddb242299366fa971c5f89ffb69d409cfe82028
Taline Kehlenbach <taline.kehlenbach@rwth-aachen.de>
2021-12-10 15:34:36 +0100

    fixed dataset extraction (deleted venue and author data)

--- citations/preprocessing/convert_to_sql.py
+++ citations/preprocessing/convert_to_sql.py
@@ -1,14 +1,46 @@
+import numpy as np
 import pandas as pd
 import json
 import sqlite3
 import glob
 
-num_files = len(glob.glob("../citation-network-dataset/split-dataset/" + "*v13.json"))
-
 # Create database
 db_name = "../citation-network-dataset/dblp.db"
 db = open(db_name, 'w')
 db.close()
+
+# Connect to database
+conn = sqlite3.connect(db_name)
+c = conn.cursor()
+
+# Define table
+columns = ['_id', 'title', #'venue',
+           'year', 'keywords',
+           'n_citation', 'lang', #'authors',
+           'fos', 'page_start',
+           'page_end', 'volume', 'issue', 'issn', 'isbn',
+           'doi', 'pdf', 'url', 'abstract', 'ref']
+
+datatypes = ['TEXT', 'TEXT', #'TEXT',
+             'INT', 'TEXT[]',
+             'INT', 'TEXT', #'TEXT',
+             'TEXT', 'TEXT',
+             'TEXT', 'TEXT', 'TEXT', 'TEXT', 'TEXT',
+             'TEXT', 'TEXT', 'TEXT[]', 'TEXT', 'TEXT[]']
+
+table_name = 'full_data'
+
+create_table = '''CREATE TABLE IF NOT EXISTS ''' + table_name + '''('''
+
+for i in range(len(columns[:-1])):
+    create_table += columns[i] + ' ' + datatypes[i] + ','
+create_table += columns[-1] + ' ' + datatypes[-1] + ''')'''
+
+c.execute(create_table)
+conn.commit()
+
+# Write to table
+num_files = len(glob.glob("../citation-network-dataset/split-dataset/" + "*v13.json"))
 
 for file in range(0, num_files):
     # Open JSON data
@@ -19,11 +51,25 @@ for file in range(0, num_files):
 
     # Create a DataFrame From the JSON Data
     df = pd.DataFrame(data)
-    df = df.applymap(str)
     df = df.rename(columns={'references': 'ref'})
+    df = df.applymap(str)
+    df['year'] = df['year'].astype(float)
+    df['year'] = df['year'].replace(np.NaN, -1)
+    df['year'] = df['year'].astype(int)
+    df['n_citation'] = df['n_citation'].astype(float)
+    df['n_citation'] = df['n_citation'].replace(np.NaN, -1)
+    df['n_citation'] = df['n_citation'].astype(int)
+    df['ref'] = df['ref'].replace(np.NaN, '')
+    df = df.drop(['venue', 'authors'], axis=1)
+    #print(type(df.loc[1, 'venue']))
 
     # Write to Database
-    conn = sqlite3.connect(db_name)
-    table_name = "table" + str(file)
-    df.to_sql(table_name, conn)
+    number_cols = len(columns) - 1
+    insert_command = '''INSERT OR IGNORE INTO ''' + table_name + ''' VALUES (''' + number_cols * '''?,''' + '''?)'''
+    for idx, row in df.iterrows():
+        c.execute(insert_command, row.to_list())
+    conn.commit()
     print("added table ", file+1, " out of ", num_files)
+
+c.close()
+conn.close()
--- citations/preprocessing/read_sql.py
+++ citations/preprocessing/read_sql.py
@@ -11,6 +11,7 @@ def convert_string_to_list(text):
 
 
 name = "../citation-network-dataset/dblp.db"
+output = "../citation-network-dataset/reference_data.feather"
 conn = sqlite3.connect(name)
 c = conn.cursor()
 
@@ -18,33 +19,9 @@ c = conn.cursor()
 columns = "_id, year, ref"
 
 # Define the name of the new table
-table = 'complete_data'
+table = 'full_data'
 select_command = 'SELECT ' + columns + ' FROM ' + str(table)
-c.execute(select_command)
-data = c.fetchall()
-
-datatypes = ['TEXT', 'INT', 'TEXT[]']
-
-for row in data:
-    print(row)
-    row_listed = list(row)
-    for i in range(len(row_listed)):
-        print(i, row_listed[i])
-        if i in (3, 5, 11, 12):
-            if row_listed[i] in nulltypes:
-                row_listed[i] = row_listed[i].replace(row_listed[i], '-1')
-            row_listed[i] = int(float(row_listed[i]))
-        elif i in (4, 8, 17, 19):
-            row_listed[i] = convert_string_to_list(row[i])
-        print(row_listed[i])
-
-    row = tuple(row_listed)
-
-
-
-c.close()
+df = pd.read_sql(select_command, conn)
+print(df)
+#df.to_feather(output)
 conn.close()
-
-
-df = pd.read_sql(command, conn)
-df.to_feather(output)
